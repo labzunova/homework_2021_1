fbb3a30bdfa4abecc3ff8c96c23bf69df3cccc12
Labzunova <dasha.labzunova@mail.ru>
2021-02-12 15:15:57 +0300

    edits after 2nd review 2.0

--- source/format.js
+++ source/format.js
@@ -13,17 +13,6 @@ const format = (numbers, cols) => {
 		throw new TypeError('wrong input');
 	}
 
-	// let colsWidth = Array(cols).fill(0);
-	// return colsWidth;
-	// colsWidth = colsWidth.map(function callback(num, i, colsWidth) {
-	// 	const column = i;
-	// 	let max = 0;
-	// 	for(let j = column; j < numbers.length; j + cols) {
-	// 		max = 1;
-	// 	} 
-	// 	return max;
-	// });
-
 	const colsWidth = Array(cols).fill(0); // определяем ширину каждой колонки
 	for(let i = 0; i < numbers.length; i++) {
 		colsWidth[i % cols] =  Math.max((numbers[i] + '').length, colsWidth[i % cols]);
@@ -34,7 +23,7 @@ const format = (numbers, cols) => {
 		let column = i % cols; // определяем, из какой колонки число
 		n = colsWidth[column] - (number + '').length;
 		if (column) {
-			n++;
+			n++; 
 		}
 
 		let nSpaces = ' '; // формируем нужный отступ
